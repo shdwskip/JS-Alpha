8411202bc546540eeba174ff2f40ee84ab98046c
Pavel Denev <denev.pavel@gmail.com>
2017-11-25 12:51:40 +0200

    Lexicographical compare task

---
+++ Arrays/lexicographCompare.js
@@ -0,0 +1,32 @@
+function charArrs(arr) {
+
+    var firstArr = arr[0],
+        secondArr = arr[1],
+        char,
+        shorter;
+
+    if (firstArr < secondArr) {
+        shorter = firstArr;
+    } else {
+        shorter = secondArr;
+    }
+
+    for (char = 0, len = shorter.length; char < len; char += 1) {
+        if (firstArr[char] < secondArr[char]) {
+            return '<';
+        } else if (firstArr[char] > secondArr[char]) {
+            return '>';
+        }
+    }
+    if (firstArr.length < secondArr.length) {
+        return '<';
+    } else if (secondArr.length < firstArr.length) {
+        return '>';
+    } else {
+        return '=';
+    }
+
+}
+let arr = ['hello', 'helloz'];
+
+charArrs(arr);

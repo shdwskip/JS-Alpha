36c193256811f4fb3251ef9c7f253ab66aa32b66
Pavel Denev <denev.pavel@gmail.com>
2017-11-26 12:35:23 +0200

    Binary Search solution added

---
+++ Arrays/7. Binary Search/binarySearch.js
@@ -0,0 +1,24 @@
+function solve(arr) {
+    let len = +arr.shift(),
+        numberX = +arr.pop(),
+        minIndex = 0,
+        maxIndex = len - 1;
+
+    while (minIndex <= maxIndex) {
+        let middleIndex = Math.floor((minIndex + maxIndex) / 2);
+        if (+arr[middleIndex] === numberX) {
+            console.log(middleIndex);
+            break;
+        } else if (+arr[middleIndex] < numberX) {
+            minIndex = middleIndex + 1;
+        } else {
+            maxIndex = middleIndex - 1;
+        }
+    }
+
+    if (maxIndex < minIndex) {
+        console.log(-1);
+    }
+}
+
+solve(['10', '1', '2', '4', '8', '16', '31', '32', '64', '77', '99', '32']);

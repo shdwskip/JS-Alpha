11cedec93b51ff86b4508786292abdd51884cefb
Pavel Denev <denev.pavel@gmail.com>
2017-11-28 11:29:40 +0200

    Associative arrays practice

---
+++ Arrays/Count Words - Associative Arrays/countWords.js
@@ -0,0 +1,28 @@
+const sentence = "Hello, how are you? Hello, are you doing well? Some BLA bLa!!";
+
+const occurrences = {};
+
+// remove all non-word characters:
+const onlyWords = sentence.split(/\W/g);
+
+for(const word of onlyWords){
+    
+    
+    const wordToLower = word.toLowerCase();
+
+    if (wordToLower === "") {
+        continue;
+    }
+    
+    if (occurrences[wordToLower] === undefined ) {
+        occurrences[wordToLower] = [];
+    }
+
+    if (occurrences[wordToLower].includes(word)) {
+        continue;
+    }
+    occurrences[wordToLower].push(word); 
+}
+
+console.log(occurrences);
+

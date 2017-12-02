59534fe4561cdb5a2d7cdef7d5010ac09d0880a5
Pavel Denev <denev.pavel@gmail.com>
2017-12-02 17:58:44 +0200

    maxSum calculated in the begining

--- Arrays/Max-Sum-3x3/max-sum-3x3.js
+++ Arrays/Max-Sum-3x3/max-sum-3x3.js
@@ -11,22 +11,20 @@ const areaRow = 3;
 const areaCol = 3;
 const maxAllowedRow = N - areaRow + 1;
 const maxAllowedCol = M - areaCol + 1;
-
 const matrix = [];
-for (let i = 0; i < N; i += 1) {
-    matrix[i] = gets().split(' ').map(Number);
-}
-print('='.repeat(10));
-
 
 let currentSum = 0;
 let maxSum = 0;
-/* 5 5
- 1  1  3  3  5
--6 -7  2 -3 -1
- 3  0 -4  5  9
- 7 -7  0  1  0
--7 -6 -4 -4  9 */
+
+for (let row = 0; row < N; row += 1) {
+    matrix[row] = gets().split(' ').map(Number);
+    for (let col = 0; col < M; col += 1) {
+        if (row < areaRow && col < areaCol) {
+            maxSum += matrix[row][col];
+        }
+    }
+}
+
 for (let row = 0; row < maxAllowedRow; row += 1) {
     for (let col = 0; col < maxAllowedCol; col += 1) {
         for (let i = 0; i < areaRow; i += 1) {
@@ -42,4 +40,4 @@ for (let row = 0; row < maxAllowedRow; row += 1) {
     }
 }
 
-print('Max:' + maxSum);
+print(maxSum);

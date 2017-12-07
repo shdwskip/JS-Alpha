74cae2bd9bde87f23c51ddffcda2a0f945db3659
Pavel Denev <denev.pavel@gmail.com>
2017-12-07 16:54:02 +0200

    maze half solved

--- Recursions/exit-maze.js
+++ Recursions/exit-maze.js
@@ -6,7 +6,7 @@ const matrix = [
     ['X', ' ', 'X', ' ', ' ', 'S', 'X', ' ', 'X', 'X', 'X'],
     [' ', ' ', 'X', ' ', 'X', ' ', ' ', ' ', ' ', 'X', 'X'],
     ['X', 'X', 'X', ' ', ' ', 'X', ' ', 'X', ' ', ' ', ' '],
-    ['X', ' ', ' ', 'X', ' ', ' ', 'X', 'X', ' ', 'X', ' '],
+    ['X', ' ', ' ', 'X', ' ', ' ', 'X', 'X', ' ', 'X', 'X'],
     ['X', 'X', 'X', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X'],
 ];
 
@@ -16,30 +16,71 @@ const startCol = (matrix[0].length - 1) / 2;
 // Moving directions will be right -> down -> left -> up
 const rowDirs = [0, +1, 0, -1];
 const colDirs = [+1, 0, -1, 0];
+const pathHolder = ['R', 'D', 'L', 'U'];
 
 const getOut = (maze) => {
-    const findPath = (row, col, direction, arr, currentPath, wayOut) => {
-        const endRow = arr.length - 1;
-        const endCol = arr[row].length - 1;
-        const currentCell = arr[row][col];
+    const findPath = (row, col, dir, source, currentPath, wayOut) => {
+        // GOING FORWARD
+        const endRow = source.length - 1;
+        const endCol = source[row].length - 1;
         if (row === endRow || row === 0 || col === endCol || col === 0) {
             // EXIT FROM THE MAZE
             wayOut.push([...currentPath]);
             return;
         }
 
-        if (currentCell === 'X' || currentCell === '@') {
-            direction += 1;
-            direction %= 4;
+        const firstDir = dir;
+        while (true) {
+            const nextRow = row + rowDirs[dir];
+            const nextCol = col + colDirs[dir];
+            if (source[nextRow][nextCol] === ' ') {
+                row += rowDirs[dir];
+                col += colDirs[dir];
+                source[row][col] = '@';
+                currentPath.push(`${pathHolder[dir]} -> `);
+                break;
+            } else {
+                dir += 1;
+                dir %= 4;
+                if (dir === firstDir) {
+                    return;
+                }
+            }
         }
-
-        arr[row][col] = '@';
-        row += rowDirs[direction];
-        col += colDirs[direction];
+        findPath(row, col, dir, source, currentPath, wayOut);
+        // GOING BACKWARDS
+        const dirHold = dir;
+        while (true) {
+            const nextRow = row + rowDirs[dir];
+            const nextCol = col + colDirs[dir];
+            if (nextRow > endRow || nextRow < 0 || nextCol > endCol || nextCol < 0 ||
+                source[nextRow][nextCol] !== ' ') {
+                dir += 1;
+                dir %= 4;
+                if (dir === dirHold) {
+                    currentPath.pop();
+                    source[row][col] = 'W';
+                    break;
+                }
+            } else {
+                source[row][col] = 'W';
+                findPath(row, col, dir, source, currentPath, wayOut);
+            }
+        }
     };
+
     const wayOut = [];
     findPath(startRow, startCol, 0, maze, [], wayOut);
-    return wayOut;
+    for (const path of wayOut) {
+        let line = '';
+        for (const dir of path) {
+            line += dir;
+        }
+        line += 'EXIT MADAFAKA!';
+        console.log(line);
+    }
+
+    // return wayOut;
 };
 
 getOut(matrix);
--- Recursions/generate-maze.js
+++ Recursions/generate-maze.js
@@ -12,8 +12,6 @@ for (let row = 0; row < rows; row += 1) {
         const randomNum = 10 * Math.random();
         if (randomNum >= 1 && randomNum <= 3 || randomNum > 6) {
             maze[row][col] = 'X';
-        } else if (randomNum > 3 && randomNum <= 6) {
-            maze[row][col] = ' ';
         } else {
             maze[row][col] = ' ';
         }

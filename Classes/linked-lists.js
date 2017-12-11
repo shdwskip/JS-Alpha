daa48d56f0278708f90b0176f68daf03ccaf694d
Pavel <denev.pavel@gmail.com>
2017-12-11 16:50:33 +0200

    prepend and inser methods added

--- Classes/linked-lists.js
+++ Classes/linked-lists.js
@@ -17,16 +17,24 @@ class LinkedList {
     get length() {
         return this._length;
     }
+    get first() {
+        return this.head.value;
+    }
+    get last() {
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+
+        return current.value;
+    }
 
     set length(newLength) {
         this._length = newLength;
     }
 
-    // get first() {
-    //     return this[0][0];
-    // }
-
-    append2(...value) {
+    // DONCHO'S SOLUTION FOR APPEND
+    append(...value) {
         if (value.length === 1) {
             const node = new ListNode(value[0]);
 
@@ -45,7 +53,8 @@ class LinkedList {
         return this;
     }
 
-    append(...value) {
+    // MINE SOLUTION FOR APPEND
+    append2(...value) {
         const node = new ListNode(value[0]);
         if (this.head === null) {
             this.head = node;
@@ -55,32 +64,70 @@ class LinkedList {
                 this.tail = this.tail.next;
             }
         } else {
-            // let current = this.head;
-            // while (current.next) {
-            //     current = current.next;
-            // }
-
             for (let i = 0; i < value.length; i += 1) {
                 this.tail.next = new ListNode(value[i]);
                 this.tail = this.tail.next;
             }
         }
-        // for (let i = 0; i < args.length; i += 1) {
-        //     this._head[this.length] = args[i];
-        //     this._length += 1;
-        // }
         this.length += value.length;
         return this;
     }
 
-    // prepend(value) {
-    //     const node = new ListNode(value);
+    prepend(...value) {
+        if (value.length === 1) {
+            const node = new ListNode(value[0]);
+            if (this.head === null) {
+                this.head = node;
+                this.tail = node;
+                this.length += 1;
+            } else {
+                node.next = this.head;
+                this.head = node;
+                this.length += 1;
+            }
+        } else {
+            for (let i = value.length - 1; i >= 0; i -= 1) {
+                this.prepend(value[i]);
+            }
+        }
+        return this;
+    }
 
-    // }
+    insert(index, ...value) {
+        if (!this.head) {
+            this.head = new ListNode(value[0]);
+        } else {
+            let parent = null;
+            let current = this.head;
+            let counter = 0;
+
+            while (counter < index) {
+                parent = current;
+                current = current.next;
+                counter += 1;
+            }
+
+            for (let i = value.length - 1; i >= 0; i -= 1) {
+                if (current) {
+                    const child = new ListNode(current.value);
+                    child.next = current.next;
+                    current.value = value[i];
+                    current.next = child;
+                    this.length += 1;
+                } else {
+                    parent.next = new ListNode(value[i]);
+                    this.length += 1;
+                }
+            }
+        }
+        return this;
+    }
 }
 
 const list = new LinkedList();
-list.append2(7).append2(6, 5, 4);
+list.append(1, 4, 5).insert(1, 2, 3);
 
 console.dir(list);
-console.log(list.length);
+// console.log(list.length);
+// console.log(list.first);
+console.log(list.last);

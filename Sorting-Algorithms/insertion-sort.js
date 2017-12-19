a32ae7f23a4caad7b71ee37e196ee63551585da7
Pavel <denev.pavel@gmail.com>
2017-12-19 14:10:41 +0200

    insertion sort

---
+++ Sorting-Algorithms/insertion-sort.js
@@ -0,0 +1,17 @@
+// O(n)
+
+const randomArr = require('./selection-sort');
+const insertionSort = (arr) => {
+    for (let i = 1; i < arr.length; i += 1) {
+        let j = i;
+        while (j > 0 && arr[j] < arr[j - 1]) {
+            [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+            j -= 1;
+        }
+    }
+};
+
+const arr = randomArr(1, 11);
+console.log(arr);
+insertionSort(arr);
+console.log(arr);

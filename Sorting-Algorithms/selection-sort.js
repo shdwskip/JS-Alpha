ea4be7db8332e0d125437fa1ca883d3c82312573
Pavel <denev.pavel@gmail.com>
2017-12-19 12:54:01 +0200

    added random generate array function

--- Sorting-Algorithms/selection-sort.js
+++ Sorting-Algorithms/selection-sort.js
@@ -1,3 +1,20 @@
+Math.randomInt = (min, max) => Math.trunc(Math.random() * (max - min) + min);
+
+const generateRandomArray = (min, max) => {
+    const result = [];
+    for (let i = min; i < max; i += 1) {
+        result.push(i);
+    }
+
+    for (let i = 0; i < result.length - 1; i += 1) {
+        const randomIndex = Math.randomInt(i + 1, result.length);
+        [result[randomIndex], result[i]] = [result[i], result[randomIndex]];
+    }
+
+    return result;
+};
+
+// O(n^2)
 const selectionSort = (arr) => {
     for (let i = 0; i < arr.length; i += 1) {
         let bestIndex = i;
@@ -13,4 +30,7 @@ const selectionSort = (arr) => {
     }
 };
 
-// O(n^2)
+const arr = generateRandomArray(1, 11);
+console.log(arr);
+selectionSort(arr);
+console.log(arr);
